f7efb48b9d0fb3701b791fba7d16447974fa5864
James <jamberin@gmail.com>
2018-05-06 18:31:47 -0400

    20180516 | Initial layout completed, designed as speced, will need more elements for send message portion

--- ContactApplication/src/ContentUpload.java
+++ ContactApplication/src/ContentUpload.java
@@ -1,9 +1,17 @@
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Label;
 
 public class ContentUpload {
 
 	protected Shell shlContentUpload;
+	private Text txtBrowse;
+	private Text txtUpload;
 
 	/**
 	 * Launch the application.
@@ -38,9 +46,41 @@ public class ContentUpload {
 	 */
 	protected void createContents() {
 		shlContentUpload = new Shell();
-		shlContentUpload.setSize(450, 300);
+		shlContentUpload.setSize(618, 409);
 		shlContentUpload.setText("Content Upload");
+		
+		Combo combo = new Combo(shlContentUpload, SWT.NONE);
+		combo.setBounds(10, 10, 582, 23);
+		combo.setText("Message Name");
+		
+		Browser brwsContentViewer = new Browser(shlContentUpload, SWT.NONE);
+		brwsContentViewer.setBounds(10, 39, 582, 230);
+		
+		Button btnBrowse = new Button(shlContentUpload, SWT.NONE);
+		btnBrowse.setBounds(10, 275, 75, 25);
+		btnBrowse.setText("Browse");
+		
+		txtBrowse = new Text(shlContentUpload, SWT.BORDER);
+		txtBrowse.setBounds(91, 277, 501, 21);
+		
+		txtUpload = new Text(shlContentUpload, SWT.BORDER);
+		txtUpload.setBounds(91, 304, 501, 21);
+		
+		Label lblMessageName = new Label(shlContentUpload, SWT.NONE);
+		lblMessageName.setBounds(48, 307, 37, 15);
+		lblMessageName.setText("Name");
+		
+		Button btnUpload = new Button(shlContentUpload, SWT.NONE);
+		btnUpload.setBounds(10, 331, 75, 25);
+		btnUpload.setText("Upload");
+		
+		Button btnCancel = new Button(shlContentUpload, SWT.NONE);
+		btnCancel.setBounds(91, 331, 75, 25);
+		btnCancel.setText("Cancel");
+		
+		Button btnNewButton = new Button(shlContentUpload, SWT.NONE);
+		btnNewButton.setBounds(517, 331, 75, 25);
+		btnNewButton.setText("Delete");
 
 	}
-
 }
--- ContactApplication/src/SendMessage.java
+++ ContactApplication/src/SendMessage.java
@@ -1,9 +1,20 @@
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class SendMessage {
 
 	protected Shell shlSendMessage;
+	private Text txtUserCount;
+	private Text txtSingleEmail;
 
 	/**
 	 * Launch the application.
@@ -38,8 +49,55 @@ public class SendMessage {
 	 */
 	protected void createContents() {
 		shlSendMessage = new Shell();
-		shlSendMessage.setSize(450, 300);
+		shlSendMessage.setSize(690, 431);
 		shlSendMessage.setText("Send Message");
+		
+		Combo cmbSelectMessage = new Combo(shlSendMessage, SWT.NONE);
+		cmbSelectMessage.setBounds(10, 10, 654, 23);
+		cmbSelectMessage.setText("Select Message");
+		
+		Browser brwsPreview = new Browser(shlSendMessage, SWT.NONE);
+		brwsPreview.setBounds(10, 39, 654, 171);
+		
+		Group grpUserSelection = new Group(shlSendMessage, SWT.NONE);
+		grpUserSelection.setText("User Selection");
+		grpUserSelection.setBounds(10, 216, 654, 136);
+		
+		Combo cmbUserQueries = new Combo(grpUserSelection, SWT.NONE);
+		cmbUserQueries.setBounds(10, 27, 104, 23);
+		cmbUserQueries.setText("User Queries");
+		
+		txtUserCount = new Text(grpUserSelection, SWT.BORDER);
+		txtUserCount.setEditable(false);
+		txtUserCount.setBounds(10, 56, 104, 21);
+		
+		Label lblUserCount = new Label(grpUserSelection, SWT.NONE);
+		lblUserCount.setBounds(120, 59, 69, 15);
+		lblUserCount.setText("User Count");
+		
+		txtSingleEmail = new Text(grpUserSelection, SWT.BORDER);
+		txtSingleEmail.setBounds(10, 83, 104, 21);
+		
+		Label lblSingleEmail = new Label(grpUserSelection, SWT.NONE);
+		lblSingleEmail.setBounds(120, 86, 80, 15);
+		lblSingleEmail.setText("Single Email");
+		
+		Button btnSendMessage = new Button(shlSendMessage, SWT.NONE);
+		btnSendMessage.setBounds(10, 358, 98, 25);
+		btnSendMessage.setText("Send Message");
+		
+		Button btnSendSample = new Button(shlSendMessage, SWT.NONE);
+		btnSendSample.setBounds(114, 358, 98, 25);
+		btnSendSample.setText("Send Sample");
+		
+		Button btnNewButton = new Button(shlSendMessage, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(218, 358, 98, 25);
+		btnNewButton.setText("Cancel");
 
 	}
 
